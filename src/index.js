967e8eaa8fc7be8c5b9ba3d4dee150c8d93fb867
kishenga <84302750+kishenga@users.noreply.github.com>
2021-06-27 13:49:14 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favorite fruits</h1>
+    <ul>
+      <li> Mango</li>
+      <li> Honeydew</li>
+      <li>Cucumber</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
